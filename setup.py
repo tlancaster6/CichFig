23cf0d760890ab207c2c7eae5b64c52c58225a10
Tucker L <tuckerlancaster@gmail.com>
2020-03-16 11:42:34 -0400

    starting a setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='CichFig',
+      version='1.0.0',
+      description='standalone plotting suite for the cichlid bower project',
+      author='Tucker Lancaster',
+      author_email='tuckerlancaster@gmail.com',
+      packages=['seaborn', 'scikit-image']
+      )
--- tester.py
+++ tester.py
@@ -1,5 +1,6 @@
 from cich_fig import CichFig as CF
 
 cf = CF()
-cf.add_projects('MC6_5')
-cf.plot_all()
+ready = cf.identify_projects()
+# cf.add_projects('MC6_5')
+# cf.plot_all()
